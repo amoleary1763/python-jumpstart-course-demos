c5fb5af4198d6cae918308a0696759ec00b6ad5a
amoleary1763 <amoleary1763@gmail.com>
2024-06-06 09:44:54 -0600

    adding try it yourself folders

--- apps/03_birthday/you_try/bymyself_program.py
+++ apps/03_birthday/you_try/bymyself_program.py
@@ -5,6 +5,7 @@ def main():
     birthday = enter_birthday()
     today = datetime.date.today()
     days = birthday_delta(birthday, today)
+    print_birthday(days)
 
 def print_header():
     print("--------------------------------")
@@ -25,11 +26,20 @@ def birthday_delta(birthday, today):
     thisyear_birthday = datetime.date(today.year, birthday.month, birthday.day)
     delta = thisyear_birthday - today
     # Only want the days to output
-    print(delta.days)
+    #print(delta.days)
     return delta.days
 
-def print_birthday():
-
+def print_birthday(days):
+    if days > 0:
+        print("Your birthday is {} days away!".format(days))
+    elif days < 0:
+        print("Your birthday was {} days ago!".format(abs(days)))
+    else:
+        print("OH MY GOSH HAPPY BIRTHDAY!")
 
 main()
 
+"""
+Notes: I'm still kind of confused about this main() function, it's a little different 
+way of thinking. He said there's a better way to do this so I'm looking forward to that.
+"""
